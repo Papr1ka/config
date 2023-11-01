df915ab0841def0844794c4708cc92648563a17e
joe <kirillpavlov4214@gmail.com>
2023-11-01 21:25:00 +0300

    progress

--- practice3/grammar.y
+++ practice3/grammar.y
@@ -11,26 +11,31 @@ void lexInClose();
 
 %start PROGRAM
 
-%token IDENTIFIER STRING NUMBER SEPARATOR_PLUS SEPARATOR_MINUS SEPARATOR_MUL SEPARATOR_DIV SEPARATOR_GT SEPARATOR_LT SEPARATOR_LB SEPARATOR_RB KEYWORD_CALC KEYWORD_LIST KEYWORD_VAR KEYWORD_PUSH KEYWORD_WHILE KEYWORD_DEFINE KEYWORD_RETURN
+%token<name> IDENTIFIER STRING
+%token<number_value>NUMBER
+%token SEPARATOR_PLUS SEPARATOR_MINUS SEPARATOR_MUL SEPARATOR_DIV SEPARATOR_GT SEPARATOR_LT SEPARATOR_LB SEPARATOR_RB
+%token KEYWORD_CALC KEYWORD_LIST KEYWORD_VAR KEYWORD_PUSH KEYWORD_WHILE KEYWORD_DEFINE KEYWORD_RETURN KEYWORD_CALL KEYWORD_OBJECT
 
-%type <name> STRING
-%type <name> NUMBER
-%type <name> IDENTIFIER
-
+%type<token> DTYPE KEY OPERAND
+%type<delim> OPERATIONS_SUM OPERATIONS_COMPARE OPERATIONS_MUL
+%type<token> MULTIPLICATION SUMMA EXPRESSION VALUE
 
 %union{
-    char name[100];
+    int lex;
+    int number_value;
+    char delim;
+    unsigned char name[200];
+    struct Token {
+        int value;
+        char name[200];
+        // 0 - number, 1 - string
+        char type;
+        char isIdentifier; //1 if true
+        char isAssigned; //1 if true
+    } token;
 }
 
 %%
-
-KEY:        IDENTIFIER |
-            STRING
-            ;
-
-DTYPE:      STRING |
-            NUMBER
-            ;
 
 PROGRAM:        S_EXP_LIST
                 ;
@@ -40,107 +45,197 @@ S_EXP_LIST:     S_EXP |
                 ;
 
 S_EXP:      DATA |
-            SEPARATOR_LB S_EXP_LIST SEPARATOR_RB
+            SEPARATOR_LB {printf("{\n");}
+            S_EXP_LIST
+            SEPARATOR_RB {{printf("}\n");}}
             ;
+
+KEY:        IDENTIFIER { strcpy($$.name, $1); $$.type = 2; } |
+            STRING { strcpy($$.name, $1); $$.type = 1; }
+            ;
+
+DTYPE:      STRING { strcpy($$.name, $1); $$.type = 1; } |
+            NUMBER { $$.value = $1; $$.type = 0; }
+            ;
+
 
 DATA:       CALC_EXP |
             KEYWORD_LIST SEPARATOR_LB DATA SEPARATOR_RB |
-            OBJECT
+            OBJECT |
+            KEY_VALUE_S_EXP_LIST |
+            SEPARATOR_LB KEY_VALUE_S_EXP SEPARATOR_RB
             ;
 
-OBJECT:     SEPARATOR_LB KEY_VALUE_S_EXP_LIST SEPARATOR_RB
-            ;
+OBJECT:     KEYWORD_OBJECT SEPARATOR_LB DATA SEPARATOR_RB
 
 KEY_VALUE_S_EXP_LIST:       KEY_VALUE_S_EXP |
                             KEY_VALUE_S_EXP KEY_VALUE_S_EXP_LIST
                             ;
 
-KEY_VALUE_S_EXP:        SEPARATOR_LB KEY DTYPE SEPARATOR_RB |
-                        SEPARATOR_LB KEY SEPARATOR_LB DATA SEPARATOR_RB SEPARATOR_RB
+KEY_VALUE_S_EXP:        KEY DTYPE
+                        {
+                            if ($2.type == 0)
+                            {
+                                printf("%d\n", $2.value);
+                            }
+                            else 
+                            {
+                                printf("%s\n", $2.name);
+                            };
+                        }|
+                        KEY SEPARATOR_LB DATA SEPARATOR_RB
                         ;
 
 CALC_EXP:       KEYWORD_CALC SEPARATOR_LB OPERATOR_LIST SEPARATOR_RB
                 ;
 
-OPERAND:        IDENTIFIER |
-                DTYPE
+OPERAND:        IDENTIFIER { strcpy($$.name, $1); $$.type = 2; } |
+                DTYPE {$$ = $1;}
                 ;
 
-EXPRESSION:     SUMMA |
+EXPRESSION:     SUMMA { $$ = $1; } |
                 EXPRESSION OPERATIONS_COMPARE SUMMA
+                {
+                if ($3.type == 0 && $1.type == 0)
+                {
+                    printf("Sep: %d\n", $2);
+                    if ($2 == 3)
+                    {
+                        $1.value = $1.value > $3.value;
+                    }
+                    else
+                    {
+                        $1.value = $1.value < $3.value;
+                    }
+                    $$ = $1;
+                }
+                else 
+                {
+                    printf("Ошибка, сравнивать можно только числа\n");
+                };
+            }
                 ;
 
-SUMMA:      MULTIPLICATION |
+SUMMA:      MULTIPLICATION { $$ = $1; } |
             SUMMA OPERATIONS_SUM MULTIPLICATION
+            {
+                if ($3.type == 0 && $1.type == 0)
+                {
+                    printf("Sep: %d\n", $2);
+                    if ($2 == 1)
+                    {
+                        $1.value = $1.value + $3.value;
+                    }
+                    else
+                    {
+                        $1.value = $1.value - $3.value;
+                    }
+                    $$ = $1;
+                }
+                else if ($3.type == 1 && $1.type == 1)
+                {
+                    if ($2 == 3)
+                    {
+                        strcat($1.name, $3.name);
+                        $$ = $1;
+                    }
+                    else
+                    {
+                        printf("Ошибка, строки нельзя вычитать\n");
+                    }
+                }
+                else 
+                {
+                    printf("Ошибка, операции сложения применимы только к числам\n");
+                };
+            }
             ;
 
-MULTIPLICATION:     OPERAND |
+MULTIPLICATION:     OPERAND { $$ = $1; } |
                     MULTIPLICATION OPERATIONS_MUL OPERAND
+                    {
+                        if ($3.type == 0 && $1.type == 0)
+                        {
+                            printf("Sep: %d\n", $2);
+                            if ($2 == 5)
+                            {
+                                $1.value = $1.value * $3.value;
+                            }
+                            else
+                            {
+                                $1.value = $1.value / $3.value;
+                            }
+                            $$ = $1;
+                        }
+                        else
+                        {
+                            printf("Ошибка, операции умножения применимы только к числам\n");
+                        };
+                    }
                     ;
 
-OPERATOR_LIST:      OPERATOR |
-                    OPERATOR OPERATOR_LIST
+OPERATOR_LIST:      SEPARATOR_LB OPERATOR SEPARATOR_RB |
+                    SEPARATOR_LB OPERATOR SEPARATOR_RB OPERATOR_LIST
                     ;
 
-OPERATOR_RETURN:        SEPARATOR_LB KEYWORD_RETURN OPERAND SEPARATOR_RB
+OPERATOR_RETURN:        KEYWORD_RETURN SEPARATOR_LB VALUE SEPARATOR_RB
 
-OPERATOR_INSIDE_DEFINE_LIST:        OPERATOR_INSIDE_DEFINE |
-                                    OPERATOR_INSIDE_DEFINE OPERATOR_INSIDE_DEFINE_LIST
-                                    ;
-
-OPERATOR_INSIDE_DEFINE:     OPERATOR_ASSIGNMENT |
-                            KEYWORD_WHILE |
-                            OPERATOR_CALL |
-                            OPERATOR_PUSH
-                            ;
-
-OPERATOR_INSIDE:        OPERATOR_ASSIGNMENT |
-                        KEYWORD_WHILE |
-                        OPERATOR_CALL |
-                        OPERATOR_PUSH |
-                        ;
 
 OPERATOR:       OPERATOR_ASSIGNMENT |
                 OPERATOR_DEFINE |
                 OPERATOR_WHILE |
-                OPERATOR_CALL
+                OPERATOR_CALL |
+                OPERATOR_PUSH
                 ;
 
 IDENTIFIER_LIST:        IDENTIFIER |
                         IDENTIFIER IDENTIFIER_LIST
                         ;
 
-OPERATOR_PUSH:      SEPARATOR_LB KEYWORD_PUSH EXPRESSION SEPARATOR_RB
+OPERATOR_PUSH:      KEYWORD_PUSH SEPARATOR_LB VALUE SEPARATOR_RB
                     ;
 
-OPERATOR_CALL:      IDENTIFIER SEPARATOR_LB IDENTIFIER_LIST SEPARATOR_RB
+OPERATOR_CALL:      KEYWORD_CALL IDENTIFIER CALL
                     ;
 
-OPERATOR_WHILE:     KEYWORD_WHILE SEPARATOR_LB OPERAND OPERATIONS_COMPARE OPERAND SEPARATOR_RB SEPARATOR_LB OPERATOR_INSIDE SEPARATOR_RB
+CALL:       SEPARATOR_LB IDENTIFIER_LIST SEPARATOR_RB
+            ;
+
+OPERATOR_WHILE:     KEYWORD_WHILE SEPARATOR_LB EXPRESSION SEPARATOR_RB SEPARATOR_LB OPERATOR_LIST SEPARATOR_RB
                     ;
 
-OPERATOR_ASSIGNMENT:        KEYWORD_VAR IDENTIFIER SEPARATOR_LB EXPRESSION SEPARATOR_RB
+VALUE:      EXPRESSION { $$ = $1;}|
+            IDENTIFIER { $$ = $1; }|
+            OPERATOR_CALL { $$ = $1; }
+            ;
+
+OPERATOR_ASSIGNMENT:        KEYWORD_VAR IDENTIFIER SEPARATOR_LB VALUE SEPARATOR_RB
+                            {
+                                ;
+                            }
                             ;
 
-OPERATOR_DEFINE:        KEYWORD_DEFINE IDENTIFIER SEPARATOR_LB IDENTIFIER_LIST SEPARATOR_RB SEPARATOR_LB OPERATOR_INSIDE_DEFINE_LIST OPERATOR_RETURN SEPARATOR_RB
+OPERATOR_DEFINE:        KEYWORD_DEFINE IDENTIFIER SEPARATOR_LB IDENTIFIER_LIST SEPARATOR_RB SEPARATOR_LB OPERATOR_LIST OPERATOR_RETURN SEPARATOR_RB |
+                        KEYWORD_DEFINE IDENTIFIER SEPARATOR_LB IDENTIFIER_LIST SEPARATOR_RB SEPARATOR_LB SEPARATOR_LB OPERATOR_RETURN SEPARATOR_RB SEPARATOR_RB |
                         ;
 
-OPERATIONS_SUM:     SEPARATOR_PLUS |
-                    SEPARATOR_MINUS
+OPERATIONS_SUM:     SEPARATOR_PLUS { $$ = 1; } |
+                    SEPARATOR_MINUS { $$ = 2; }
                     ;
 
-OPERATIONS_COMPARE:     SEPARATOR_GT |
-                        SEPARATOR_LT
+OPERATIONS_COMPARE:     SEPARATOR_GT { $$ = 3; } |
+                        SEPARATOR_LT { $$ = 4; }
                         ;
 
-OPERATIONS_MUL:     SEPARATOR_MUL |
-                    SEPARATOR_DIV
+OPERATIONS_MUL:     SEPARATOR_MUL { $$ = 5; } |
+                    SEPARATOR_DIV { $$ = 6; }
                     ;
 
 %%
 
 int main(int argc, char **argv)
 {
+    setlocale(LC_ALL, "");
 	if(argc < 2)
 	{
 		printf("\nNot enough arguments. Please specify filename.\n");
--- practice3/lex.yy.c
+++ practice3/lex.yy.c
@@ -373,8 +373,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 21
-#define YY_END_OF_BUFFER 22
+#define YY_NUM_RULES 23
+#define YY_END_OF_BUFFER 24
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -382,14 +382,15 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[58] =
+static const flex_int16_t yy_accept[65] =
     {   0,
-        0,    0,   22,   21,    1,   21,   15,   16,   11,    9,
-       10,   12,   17,   14,   13,   18,   18,   18,   18,   18,
-       18,   18,   18,    0,   19,    0,   17,   18,   18,   18,
-       18,   18,   18,   18,   18,    0,    0,   18,   18,   18,
-       18,   18,    7,   18,   20,    5,   18,    6,    8,   18,
-       18,   18,   18,    2,    4,    3,    0
+        0,    0,   24,   23,    1,   23,   17,   18,   13,   11,
+       12,   14,   19,   16,   15,   20,   20,   20,   20,   20,
+       20,   20,   20,   20,    0,   21,    0,   19,   20,   20,
+       20,   20,   20,   20,   20,   20,   20,    0,    0,   20,
+       20,   20,   20,   20,   20,    9,   20,   22,    7,    4,
+       20,    8,   20,   10,   20,   20,   20,   20,   20,    2,
+        6,    3,    5,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -403,10 +404,10 @@ static const YY_CHAR yy_ec[256] =
         1,   13,    1,    1,   14,   14,   14,   14,   14,   14,
        14,   14,   14,   14,   14,   14,   14,   14,   14,   14,
        14,   14,   14,   14,   14,   14,   14,   14,   14,   14,
-        1,    1,    1,    1,    1,    1,   15,   14,   16,   17,
+        1,    1,    1,    1,    1,    1,   15,   16,   17,   18,
 
-       18,   19,   14,   20,   21,   14,   14,   22,   14,   23,
-       14,   24,   14,   25,   26,   27,   28,   29,   30,   14,
+       19,   20,   14,   21,   22,   23,   14,   24,   14,   25,
+       26,   27,   14,   28,   29,   30,   31,   32,   33,   14,
        14,   14,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -424,70 +425,75 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[31] =
+static const YY_CHAR yy_meta[34] =
     {   0,
         1,    1,    2,    1,    1,    1,    1,    1,    1,    1,
         3,    1,    1,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3
     } ;
 
-static const flex_int16_t yy_base[61] =
+static const flex_int16_t yy_base[68] =
     {   0,
-        0,    0,   72,   73,   73,   67,   73,   73,   73,   73,
-       73,   63,   58,   73,   73,    0,   53,   49,   45,   37,
-       46,   48,   42,   57,   73,   53,   48,    0,   36,   38,
-       30,   29,   27,   28,   31,   44,   24,   34,   28,   21,
-       27,   18,    0,   23,   37,    0,   20,    0,    0,   17,
-       23,   21,   10,    0,    0,    0,   73,   34,   29,   37
+        0,    0,   81,   82,   82,   76,   82,   82,   82,   82,
+       82,   72,   67,   82,   82,    0,   62,   57,   53,   58,
+       42,   53,   56,   49,   65,   82,   61,   56,    0,   42,
+       45,   35,   40,   33,   31,   32,   37,   51,   27,   18,
+       35,   26,   36,   33,   22,    0,   28,   44,    0,    0,
+       25,    0,   32,    0,   19,   22,   21,    9,   13,    0,
+        0,    0,    0,   82,   42,   33,   45
     } ;
 
-static const flex_int16_t yy_def[61] =
+static const flex_int16_t yy_def[68] =
     {   0,
-       57,    1,   57,   57,   57,   58,   57,   57,   57,   57,
-       57,   57,   57,   57,   57,   59,   59,   59,   59,   59,
-       59,   59,   59,   58,   57,   60,   57,   59,   59,   59,
-       59,   59,   59,   59,   59,   60,   60,   59,   59,   59,
-       59,   59,   59,   59,   60,   59,   59,   59,   59,   59,
-       59,   59,   59,   59,   59,   59,    0,   57,   57,   57
+       64,    1,   64,   64,   64,   65,   64,   64,   64,   64,
+       64,   64,   64,   64,   64,   66,   66,   66,   66,   66,
+       66,   66,   66,   66,   65,   64,   67,   64,   66,   66,
+       66,   66,   66,   66,   66,   66,   66,   67,   67,   66,
+       66,   66,   66,   66,   66,   66,   66,   67,   66,   66,
+       66,   66,   66,   66,   66,   66,   66,   66,   66,   66,
+       66,   66,   66,    0,   64,   64,   64
     } ;
 
-static const flex_int16_t yy_nxt[104] =
+static const flex_int16_t yy_nxt[116] =
     {   0,
         4,    5,    5,    6,    7,    8,    9,   10,   11,   12,
-       13,   14,   15,   16,   16,   17,   18,   16,   16,   16,
-       16,   19,   16,   20,   21,   16,   16,   16,   22,   23,
-       37,   28,   56,   45,   24,   24,   24,   36,   55,   36,
-       54,   53,   52,   37,   51,   50,   49,   48,   47,   46,
-       37,   44,   43,   42,   41,   40,   39,   38,   27,   37,
-       25,   35,   34,   33,   32,   31,   30,   29,   27,   26,
-       25,   57,    3,   57,   57,   57,   57,   57,   57,   57,
-       57,   57,   57,   57,   57,   57,   57,   57,   57,   57,
-       57,   57,   57,   57,   57,   57,   57,   57,   57,   57,
+       13,   14,   15,   16,   16,   16,   17,   18,   16,   16,
+       16,   16,   16,   19,   16,   20,   21,   22,   16,   16,
+       16,   23,   24,   39,   49,   29,   48,   63,   62,   61,
+       60,   50,   25,   25,   25,   38,   59,   38,   58,   57,
+       39,   56,   55,   54,   53,   52,   51,   39,   47,   46,
+       45,   44,   43,   42,   41,   40,   28,   39,   26,   37,
+       36,   35,   34,   33,   32,   31,   30,   28,   27,   26,
+       64,    3,   64,   64,   64,   64,   64,   64,   64,   64,
+       64,   64,   64,   64,   64,   64,   64,   64,   64,   64,
 
-       57,   57,   57
+       64,   64,   64,   64,   64,   64,   64,   64,   64,   64,
+       64,   64,   64,   64,   64
     } ;
 
-static const flex_int16_t yy_chk[104] =
+static const flex_int16_t yy_chk[116] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-       37,   59,   53,   37,   58,   58,   58,   60,   52,   60,
-       51,   50,   47,   45,   44,   42,   41,   40,   39,   38,
-       36,   35,   34,   33,   32,   31,   30,   29,   27,   26,
-       24,   23,   22,   21,   20,   19,   18,   17,   13,   12,
-        6,    3,   57,   57,   57,   57,   57,   57,   57,   57,
-       57,   57,   57,   57,   57,   57,   57,   57,   57,   57,
-       57,   57,   57,   57,   57,   57,   57,   57,   57,   57,
+        1,    1,    1,   39,   40,   66,   39,   59,   58,   57,
+       56,   40,   65,   65,   65,   67,   55,   67,   53,   51,
+       48,   47,   45,   44,   43,   42,   41,   38,   37,   36,
+       35,   34,   33,   32,   31,   30,   28,   27,   25,   24,
+       23,   22,   21,   20,   19,   18,   17,   13,   12,    6,
+        3,   64,   64,   64,   64,   64,   64,   64,   64,   64,
+       64,   64,   64,   64,   64,   64,   64,   64,   64,   64,
 
-       57,   57,   57
+       64,   64,   64,   64,   64,   64,   64,   64,   64,   64,
+       64,   64,   64,   64,   64
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static const flex_int32_t yy_rule_can_match_eol[22] =
+static const flex_int32_t yy_rule_can_match_eol[24] =
     {   0,
-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 
-    0, 0,     };
+1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 1, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -511,8 +517,9 @@ int state;
 void UndefinedTokenPrint();
 int lexInOpen(char* filename);
 void lexInClose();
-#line 514 "lex.yy.c"
-#line 515 "lex.yy.c"
+int number;
+#line 521 "lex.yy.c"
+#line 522 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -729,9 +736,9 @@ YY_DECL
 		}
 
 	{
-#line 35 "./grammar.l"
+#line 38 "./grammar.l"
 
-#line 734 "lex.yy.c"
+#line 741 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -758,13 +765,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 58 )
+				if ( yy_current_state >= 65 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 73 );
+		while ( yy_base[yy_current_state] != 82 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -801,111 +808,121 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
-#line 36 "./grammar.l"
+#line 39 "./grammar.l"
 {;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 37 "./grammar.l"
-{ printf("(KEYWORD_WHILE, %s)\n", yytext); return KEYWORD_WHILE; }
+#line 40 "./grammar.l"
+{  printf("Lex: %s\n", yytext); return KEYWORD_WHILE; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 38 "./grammar.l"
-{ printf("(KEYWORD_RETURN, %s)\n", yytext); return KEYWORD_RETURN; }
+#line 41 "./grammar.l"
+{   printf("Lex: %s\n", yytext); return KEYWORD_OBJECT; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 39 "./grammar.l"
-{ printf("(KEYWORD_DEFINE, %s)\n", yytext); return KEYWORD_DEFINE; }
+#line 42 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_CALL; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 40 "./grammar.l"
-{ printf("(KEYWORD_CALC, %s)\n", yytext); return KEYWORD_CALC; }
+#line 43 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_RETURN; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 41 "./grammar.l"
-{ printf("(KEYWORD_LIST, %s)\n", yytext); return KEYWORD_LIST; }
+#line 44 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_DEFINE; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 42 "./grammar.l"
-{ printf("(KEYWORD_VAR, %s)\n", yytext); return KEYWORD_VAR; }
+#line 45 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_CALC; }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 43 "./grammar.l"
-{ printf("(KEYWORD_PUSH, %s)\n", yytext); return KEYWORD_PUSH; }
+#line 46 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_LIST; }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 44 "./grammar.l"
-{ printf("(SEPARATOR_PLUS, %s)\n", yytext);return SEPARATOR_PLUS; }
+#line 47 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_VAR; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 45 "./grammar.l"
-{ printf("(SEPARATOR_MINUS, %s)\n", yytext); return SEPARATOR_MINUS; }
+#line 48 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return KEYWORD_PUSH; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 46 "./grammar.l"
-{ printf("(SEPARATOR_MUL, %s)\n", yytext); return SEPARATOR_MUL; }
+#line 49 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_PLUS; }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 47 "./grammar.l"
-{ printf("(SEPARATOR_DIV, %s)\n", yytext);return SEPARATOR_DIV; }
+#line 50 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return SEPARATOR_MINUS; }
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 48 "./grammar.l"
-{ printf("(SEPARATOR_GT, %s)\n", yytext);return SEPARATOR_GT; }
+#line 51 "./grammar.l"
+{   printf("Lex: %s\n", yytext);return SEPARATOR_MUL; }
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 49 "./grammar.l"
-{ printf("(SEPARATOR_LT, %s)\n", yytext);return SEPARATOR_LT; }
+#line 52 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_DIV; }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 50 "./grammar.l"
-{ printf("(SEPARATOR_LB, %s)\n", yytext);return SEPARATOR_LB; }
+#line 53 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_GT; }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 51 "./grammar.l"
-{ printf("(SEPARATOR_RB, %s)\n", yytext);return SEPARATOR_RB; }
+#line 54 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_LT; }
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 52 "./grammar.l"
-{ printf("(NUMBER, %s)\n", yytext); sscanf(yytext, "%s", yylval.name); return (NUMBER); }
+#line 55 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_LB; }
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 53 "./grammar.l"
-{ printf("(IDENTIFIER, %s)\n", yytext); sscanf(yytext, "%s", yylval.name); return (IDENTIFIER); }
+#line 56 "./grammar.l"
+{  printf("Lex: %s\n", yytext);return SEPARATOR_RB; }
 	YY_BREAK
 case 19:
-/* rule 19 can match eol */
 YY_RULE_SETUP
-#line 54 "./grammar.l"
-{ printf("(STRING, %s)\n", yytext);sscanf(yytext, "%s", yylval.name); return (STRING); }
+#line 57 "./grammar.l"
+{   printf("Lex: %s\n", yytext);yylval.number_value = atoi(yytext); return (NUMBER); }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 55 "./grammar.l"
-{;}
+#line 58 "./grammar.l"
+{   printf("Lex: %s\n", yytext);strcpy(yylval.name, yytext); return (IDENTIFIER); }
 	YY_BREAK
 case 21:
+/* rule 21 can match eol */
 YY_RULE_SETUP
-#line 56 "./grammar.l"
+#line 59 "./grammar.l"
+{  printf("Lex: %s\n", yytext);strcpy(yylval.name, yytext); return (STRING); }
+	YY_BREAK
+case 22:
+YY_RULE_SETUP
+#line 60 "./grammar.l"
+{;}
+	YY_BREAK
+case 23:
+YY_RULE_SETUP
+#line 61 "./grammar.l"
 ECHO;
 	YY_BREAK
-#line 908 "lex.yy.c"
+#line 925 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1202,7 +1219,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 58 )
+			if ( yy_current_state >= 65 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1230,11 +1247,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 58 )
+		if ( yy_current_state >= 65 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 57);
+	yy_is_jam = (yy_current_state == 64);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1922,14 +1939,14 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 56 "./grammar.l"
+#line 61 "./grammar.l"
 
 
 int lexInOpen(char* filename)
 {
 	if((yyin = fopen(filename, "r")) == NULL)
 	{
-		printf("\nCannot open file %s.\n", filename);
+		
 		return -1;
 	}
 	yylineno = 1;
@@ -1943,5 +1960,5 @@ void lexInClose()
 
 void UndefinedTokenPrint()
 {
-	printf("Error: \"Нераспознанный токен\" in line %d. Token = %s\n", yylineno, yytext);
+	
 }
